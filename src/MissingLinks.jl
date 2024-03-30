6b28c400b2015d8935aee39ca0dfed9754a1443b
Matt Bhagat-Conway <matt@indicatrix.org>
2024-03-29 20:22:49 -0400

    create service areas

--- src/MissingLinks.jl
+++ src/MissingLinks.jl
@@ -1,7 +1,7 @@
 module MissingLinks
 import MetaGraphsNext: MetaGraph, labels, edge_labels, code_for, label_for, labels
 import Graphs: Graph, dijkstra_shortest_paths, nv, ne, is_directed, connected_components, strongly_connected_components, rem_vertex!,
-    outneighbors, has_edge, has_vertex, vertices
+    outneighbors, has_edge, has_vertex, vertices, edges
 import LibSpatialIndex: RTree, insert!, intersects
 import GeoInterface, ArchGDAL
 import DataFrames: DataFrame, nrow, metadata
@@ -20,8 +20,10 @@ include("score.jl")
 include("weight_nodes.jl")
 include("non_noded_graph.jl")
 include("geom.jl")
+include("service_area.jl")
+
 
 export graph_from_gdal, identify_potential_missing_links, links_to_gdf, remove_tiny_islands, deduplicate_links,
-    score_links, create_graph_weights, semi_to_fully_noded, add_short_edges!, index_graph_edges
+    score_links, create_graph_weights, semi_to_fully_noded, add_short_edges!, index_graph_edges, service_area
 
 end
