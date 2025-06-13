788b58f4e0815fe52a8a0955087d3af86ab0e923
Matt Bhagat-Conway <matt@indicatrix.org>
2025-06-13 18:11:19 -0400

    remove type piracy

--- src/geom.jl
+++ src/geom.jl
@@ -62,7 +62,7 @@ function break_long_line(line::ArchGDAL.IGeometry{ArchGDAL.wkbLineString}, max_l
     end
 end
 
-Base.reverse(g::ArchGDAL.IGeometry{ArchGDAL.wkbLineString}) =
+reverse_geom(g::ArchGDAL.IGeometry{ArchGDAL.wkbLineString}) =
     ArchGDAL.createlinestring([ArchGDAL.getpoint(g, i) for i in (ArchGDAL.ngeom(g) - 1):-1:0])
 
 "Run the provided function on each constituent geometry of a multigeometry"
