1660bdcdca608463ea77016ca636dc82055b3c41
Matt Bhagat-Conway <matt@indicatrix.org>
2025-08-21 19:41:19 -0400

    fix distance tests

--- test/dist.jl
+++ test/dist.jl
@@ -1,7 +1,21 @@
 @testitem "compute_net_distance and fill_matrix" begin
-    import MissingLinks: graph_from_gdal, compute_net_distance, fill_distance_matrix!
+    import MissingLinks: graph_from_gdal, compute_net_distance, calculate_distances, VertexID
     import DataFrames: DataFrame
     import ArchGDAL as AG
+    import Graphs: nv
+    import MetaGraphsNext: label_for
+
+    function to_mtx(dmat)
+        mtx = Matrix{Int64}(undef, nv(G), nv(G))
+        for fr in 1:nv(G)
+            for to in 1:nv(G)
+                val = dmat[label_for(dmat.graph, fr), label_for(dmat.graph, to)]
+                # recode missing to -1 so we don't have to deal with missing != missing
+                mtx[fr, to] = ismissing(val) ? -1 : val
+            end
+        end
+        return mtx
+    end
 
     #= Graph looks like this:
     1234567
@@ -24,11 +38,10 @@
         AG.createlinestring([[1.0, 7.0], [7.0, 7.0]])
     ]))
 
-    dmat = zeros(UInt16, (6, 6))
-    fill_distance_matrix!(G, dmat; maxdist=12) # 1, 4 should be too far
+    dmat = calculate_distances(G; maxdist=12) # 1, 4 should be too far
 
-    U = typemax(UInt16)
-    @test dmat ≈ [
+    U = -1
+    @test to_mtx(dmat) == [
         0  6  4  10 U U;
         6  0  10 U  U U;
         4  10 0  6  U U;
@@ -38,21 +51,21 @@
     ]
 
     # from 2 meters down 1->2 to two meters down 3->4 - 2 meters each end to access node, plus d[2, 3]
-    @test compute_net_distance(dmat, 1, 2, UInt16(2), UInt16(4), 3, 4, UInt16(2), UInt16(4)) == UInt16(2 + 4 + 2)
+    @test compute_net_distance(dmat, VertexID(1), VertexID(2), 2, 4, VertexID(3), VertexID(4), 2, 4) == 2 + 4 + 2
 
     # adjacent edges - two meters down 1->2 to three meters down 1->3 = 2 + 3
-    @test compute_net_distance(dmat, 1, 2, UInt16(2), UInt16(4), 1, 3, UInt16(3), UInt16(1)) == UInt16(2 + 3)
+    @test compute_net_distance(dmat, VertexID(1), VertexID(2), 2, 4, VertexID(1), VertexID(3), 3, 1) == 2 + 3
 
     # same edge
-    @test compute_net_distance(dmat, 1, 2, UInt16(2), UInt16(4), 1, 2, UInt16(3), UInt16(3)) == UInt16(1)
+    @test compute_net_distance(dmat, VertexID(1), VertexID(2), 2, 4, VertexID(1), VertexID(2), 3, 3) == 1
 
     # and backwards
-    @test compute_net_distance(dmat, 1, 2, UInt16(3), UInt16(3), 1, 2, UInt16(2), UInt16(4)) == UInt16(1)
+    @test compute_net_distance(dmat, VertexID(1), VertexID(2), 3, 3, VertexID(1), VertexID(2), 2, 4) == 1
 
     # disconnected
-    @test compute_net_distance(dmat, 1, 2, UInt16(2), UInt16(4), 5, 6, UInt16(2), UInt16(4)) == U
+    @test ismissing(compute_net_distance(dmat, VertexID(1), VertexID(2), 2, 4, VertexID(5), VertexID(6), 2, 4))
 end
-
+    
 @testitem "all possible combinations of compute_net_dist" begin
     # Test all possible combinations of which ends are closer.
     # The graph looks like this:
@@ -62,7 +75,7 @@ end
     #  | /
     #  |/
     #  We build it with the two angled edges in all possible orientations to make sure the correct distance is calculated
-    import MissingLinks: graph_from_gdal, compute_net_distance, fill_distance_matrix!
+    import MissingLinks: graph_from_gdal, compute_net_distance, calculate_distances, VertexID
     import DataFrames: DataFrame
     import ArchGDAL as AG
 
@@ -80,21 +93,20 @@ end
                 ]))
 
                 # true distance from endpoints should be 1 + 1 + 3
-                dmat = zeros(UInt16, (4, 4))
-                fill_distance_matrix!(G, dmat)
+                dmat = calculate_distances(G)
 
                 @test compute_net_distance(
                     dmat,
-                    1,
-                    2,
+                    VertexID(1),
+                    VertexID(2),
                     # if one is reversed, route from the start of one (the disconnected part)
                     # otherwise, the end
-                    reverse1 ? zero(UInt16) : one(UInt16),
-                    reverse1 ? one(UInt16) : zero(UInt16),
-                    3,
-                    4,
-                    reverse2 ? zero(UInt16) : one(UInt16),
-                    reverse2 ? one(UInt16) : zero(UInt16),
+                    reverse1 ? 0 : 1,
+                    reverse1 ? 1 : 0,
+                    VertexID(3),
+                    VertexID(4),
+                    reverse2 ? 0 : 1,
+                    reverse2 ? 1 : 0,
                 ) ≈ 5
             end
         end
