64a9708e46ee2d2ae8848e50920bd2941e6295c8
Matthew Wigginton Bhagat-Conway <matt@indicatrix.org>
2025-12-03 06:47:54 -0500

    @preserve all variables

--- src/spidx.jl
+++ src/spidx.jl
@@ -1,5 +1,5 @@
 # wrap LibSpatialIndex functions to work around https://github.com/JuliaGeo/LibSpatialIndex.jl/pull/36
 
-insert!(sidx::RTree, idx, min, max) = GC.@preserve min max LibSpatialIndex.insert!(sidx, idx, min, max)
+insert!(sidx::RTree, idx, min, max) = GC.@preserve min max idx LibSpatialIndex.insert!(sidx, idx, min, max)
 intersects(sidx::RTree, min, max) = GC.@preserve min max LibSpatialIndex.intersects(sidx, min, max)
 knn(sidx::RTree, min, max, n) = GC.@preserve min max LibSpatialIndex.knn(sidx, min, max, n)
